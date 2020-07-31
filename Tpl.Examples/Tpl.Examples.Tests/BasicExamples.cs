f7855e0d1a4584934ca7ab9aef279515e8a2f9d2
ballan <ballan00@gmail.com>
2020-07-30 23:34:42 -0500

    played with TransformManyBlock

--- Tpl.Examples/Tpl.Examples.Tests/BasicExamples.cs
+++ Tpl.Examples/Tpl.Examples.Tests/BasicExamples.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Tpl.Examples.Tests.Extensions;
 using Tpl.Examples.Tests.Models;
 using Tpl.Examples.Tests.Services;
 
@@ -13,6 +17,9 @@ namespace Tpl.Examples.Tests
     [TestClass]
     public class BasicExamples
     {
+        /////////////////////////
+        /// BUFFERING BLOCKS ///
+        ///////////////////////
         [TestMethod]
         public async Task BufferBlock_Example1()
         {
@@ -28,12 +35,14 @@ namespace Tpl.Examples.Tests
             {
                 Console.Out.WriteLine(customerReceived.Id);
             }
+
+            // will work without this but should call when done
+            bufferBlock.Complete();
         }
 
         [TestMethod]
         public async Task BroadcastBlock_Example1()
         {
-            // todo: try async
             var broadcastBlock = new BroadcastBlock<ImportCustomer>(null);
             var actionBlock1 = new ActionBlock<ImportCustomer>(PerformAction1);
             var actionBlock2 = new ActionBlock<ImportCustomer>(PerformAction2);
@@ -55,16 +64,103 @@ namespace Tpl.Examples.Tests
             Task.WaitAll(actionBlock1.Completion, actionBlock2.Completion);
         }
 
-        private void PerformAction2(ImportCustomer arg)
+        private async Task PerformAction2(ImportCustomer arg)
         {
-            Task.Delay(50).Wait();
-            Console.Out.WriteLine($"Action 2: {arg.Id}");
+            await Task.Delay(50);
+            await Console.Out.WriteLineAsync($"Action 2: {arg.Id}");
         }
 
-        private void PerformAction1(ImportCustomer arg)
+        private async Task PerformAction1(ImportCustomer arg)
         {
-            Task.Delay(100).Wait();
-            Console.Out.WriteLine($"Action 1: {arg.Id}");
+            await Task.Delay(100);
+            await Console.Out.WriteLineAsync($"Action 1: {arg.Id}");
+        }
+
+        /////////////////////////
+        /// EXECUTION BLOCKS ///
+        ///////////////////////
+
+        [TestMethod]
+        public async Task ActionBlock_Example1()
+        {
+            var actionBlock = new ActionBlock<ImportCustomer>(PerformAction1, new ExecutionDataflowBlockOptions()
+            {
+                // will process in order if not set
+                MaxDegreeOfParallelism = 10
+            });
+
+            var bulkService = new BulkCustomerDataService(10);
+
+            await foreach (var customer in bulkService.GetCustomersFromImport())
+            {
+                actionBlock.Post(customer);
+            }
+
+            // must call or will never complete
+            actionBlock.Complete();
+
+            // must call or test will end before finished
+            await actionBlock.Completion;
+        }
+
+        [TestMethod]
+        public async Task TransformBlock_Example1()
+        {
+            var transformBlock = new TransformBlock<ImportCustomer, Customer>(importCustomer => importCustomer.ToCustomer(), new ExecutionDataflowBlockOptions()
+            {
+                // will process in order if not set
+                MaxDegreeOfParallelism = 10
+            });
+
+            var bulkService = new BulkCustomerDataService(10);
+
+            await foreach (var importCustomer in bulkService.GetCustomersFromImport())
+            {
+                transformBlock.Post(importCustomer);
+            }
+
+            // todo: why does this not work unless you step through
+            //while (transformBlock.TryReceive(out Customer customer))
+            //{
+            //    Console.Out.WriteLine($"Received: {customer.Id} - {customer.ExternalId}");
+            //}
+
+            // if you don't receive every record then the block never completes
+            for (int i = 0; i < 10; i++)
+            {
+                var customer = await transformBlock.ReceiveAsync();
+                Console.Out.WriteLine($"Received: {customer.Id} - {customer.ExternalId}");
+            }
+
+            transformBlock.Complete();
+
+            await transformBlock.Completion;
+        }
+
+        [TestMethod]
+        public async Task TransformManyBlock_Example1()
+        {
+            var transformManyBlock = new TransformManyBlock<string, ImportCustomer>(JsonConvert.DeserializeObject<List<ImportCustomer>>, 
+                new ExecutionDataflowBlockOptions()
+                {
+                    MaxDegreeOfParallelism = 1
+                });
+
+            var bulkService = new BulkCustomerDataService(10);
+            var json = JsonConvert.SerializeObject(await bulkService.GetCustomersFromImport().ToListAsync());
+
+            transformManyBlock.Post(json);
+
+            // must receive all messages or code will never exit
+            for (int i = 0; i < 10; i++)
+            {
+                var customer = await transformManyBlock.ReceiveAsync();
+                Console.Out.WriteLine($"Received: {customer.Id}");
+            }
+
+            transformManyBlock.Complete();
+
+            await transformManyBlock.Completion;
         }
     }
 }
---
+++ Tpl.Examples/Tpl.Examples.Tests/Extensions/ImportCustomerExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tpl.Examples.Tests.Models;
+
+namespace Tpl.Examples.Tests.Extensions
+{
+    public static class ImportCustomerExtensions
+    {
+        public static Customer ToCustomer(this ImportCustomer importCustomer)
+        {
+            var customer = new Customer
+            {
+                Id = Guid.NewGuid(),
+                ExternalId = importCustomer.Id,
+                Name = {FirstName = importCustomer.FirstName, LastName = importCustomer.LastName}
+            };
+
+            if (!string.IsNullOrWhiteSpace(importCustomer.Email))
+            {
+                customer.Emails.Add(new CustomerEmail()
+                {
+                    Type = "Primary",
+                    Value = importCustomer.Email
+                });
+            }
+
+            if (!string.IsNullOrWhiteSpace(importCustomer.CellPhone))
+            {
+                customer.Phones.Add(new CustomerPhone()
+                {
+                    Type = "Cell",
+                    Value = importCustomer.CellPhone
+                });
+            }
+
+            if (!string.IsNullOrWhiteSpace(importCustomer.DayPhone))
+            {
+                customer.Phones.Add(new CustomerPhone()
+                {
+                    Type = "Day",
+                    Value = importCustomer.DayPhone
+                });
+            }
+
+            if (!string.IsNullOrWhiteSpace(importCustomer.EvePhone))
+            {
+                customer.Phones.Add(new CustomerPhone()
+                {
+                    Type = "Evening",
+                    Value = importCustomer.EvePhone
+                });
+            }
+
+            return customer;
+        }
+    }
+}
