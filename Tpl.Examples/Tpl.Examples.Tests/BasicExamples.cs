574999d8450358aa6b5df0074c6270d71b97a7b2
ballan <ballan00@gmail.com>
2020-08-15 10:32:58 -0500

    finished cleaning up examples using action blocks instead of working with blocks using raw code

--- Tpl.Examples/Tpl.Examples.Tests/BasicExamples.cs
+++ Tpl.Examples/Tpl.Examples.Tests/BasicExamples.cs
@@ -76,19 +76,6 @@ namespace Tpl.Examples.Tests
             Assert.AreEqual(200, _performActionCount, $"Action count is {_performActionCount}");
         }
 
-        private static SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
-        private static int _performActionCount = 0;
-
-        private async Task PerformAction<T>(T input, int actionId, int delayMs)
-        {
-            await _semaphore.WaitAsync();
-            _performActionCount++;
-            _semaphore.Release();
-
-            await Task.Delay(delayMs);
-            Console.WriteLine($"Action {actionId}: {JsonConvert.SerializeObject(input)}");
-        }
-
         /////////////////////////
         /// EXECUTION BLOCKS ///
         ///////////////////////
@@ -115,53 +102,55 @@ namespace Tpl.Examples.Tests
         [TestMethod]
         public async Task TransformBlock_Example1()
         {
+            _performActionCount = 0;
             var transformBlock = new TransformBlock<ImportCustomer, Customer>(importCustomer => importCustomer.ToCustomer(), ExecutionOptions);
+            var actionBlock = new ActionBlock<Customer>(customer => PerformAction(customer, 1, 1));
+            transformBlock.LinkTo(actionBlock, LinkOptions);
 
-            var bulkService = new BulkCustomerDataService(10);
+            var bulkService = new BulkCustomerDataService(100);
 
             await foreach (var importCustomer in bulkService.GetCustomersFromImport())
             {
                 transformBlock.Post(importCustomer);
             }
 
-            // todo: why does this not work unless you step through
-            //while (transformBlock.TryReceive(out Customer customer))
-            //{
-            //    Console.Out.WriteLine($"Received: {customer.Id} - {customer.ExternalId}");
-            //}
-
-            // if you don't receive every record then the block never completes
-            for (int i = 0; i < 10; i++)
-            {
-                var customer = await transformBlock.ReceiveAsync();
-                Console.Out.WriteLine($"Received: {customer.Id} - {customer.ExternalId}");
-            }
-
             transformBlock.Complete();
 
-            await transformBlock.Completion;
+            await actionBlock.Completion;
+
+            Assert.AreEqual(100, _performActionCount, $"Action count is {_performActionCount}");
         }
 
         [TestMethod]
         public async Task TransformManyBlock_Example1()
         {
             var transformManyBlock = new TransformManyBlock<string, ImportCustomer>(JsonConvert.DeserializeObject<List<ImportCustomer>>, ExecutionOptions);
+            var actionBlock = new ActionBlock<ImportCustomer>(importCustomer => PerformAction(importCustomer, 1, 1));
+            transformManyBlock.LinkTo(actionBlock, LinkOptions);
 
-            var bulkService = new BulkCustomerDataService(10);
+            var bulkService = new BulkCustomerDataService(100);
             var json = JsonConvert.SerializeObject(await bulkService.GetCustomersFromImport().ToListAsync());
 
             transformManyBlock.Post(json);
 
-            // must receive all messages or code will never exit
-            for (int i = 0; i < 10; i++)
-            {
-                var customer = await transformManyBlock.ReceiveAsync();
-                Console.Out.WriteLine($"Received: {customer.Id}");
-            }
-
             transformManyBlock.Complete();
 
-            await transformManyBlock.Completion;
+            await actionBlock.Completion;
+
+            Assert.AreEqual(100, _performActionCount, $"Action count is {_performActionCount}");
+        }
+
+        private static SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
+        private static int _performActionCount = 0;
+
+        private async Task PerformAction<T>(T input, int actionId, int delayMs)
+        {
+            await _semaphore.WaitAsync();
+            _performActionCount++;
+            _semaphore.Release();
+
+            await Task.Delay(delayMs);
+            Console.WriteLine($"Action {actionId}: {JsonConvert.SerializeObject(input)}");
         }
     }
 }
